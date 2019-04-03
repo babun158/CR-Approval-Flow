a7b2af83f55929680d224f8144a6009d29641cb1
babun158 <44605552+babun158@users.noreply.github.com>
2019-04-03 11:52:46 +0530

    CR-Approval-Flow

---
+++ src/webparts/changeRequestingPage/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IChangeRequestingPageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ChangeRequestingPageWebPartStrings' {
+  const strings: IChangeRequestingPageWebPartStrings;
+  export = strings;
+}
